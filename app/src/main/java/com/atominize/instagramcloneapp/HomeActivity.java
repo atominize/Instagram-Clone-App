a013eccaf3dc9f2f9cfb619b43d52fb0d756aa89
atominize <atominize@gmail.com>
2019-12-25 21:42:12 +0000

    Setted up bottom navigation icons and customized

--- app/src/main/java/com/atominize/instagramcloneapp/HomeActivity.java
+++ app/src/main/java/com/atominize/instagramcloneapp/HomeActivity.java
@@ -3,12 +3,27 @@ package com.atominize.instagramcloneapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.atominize.instagramcloneapp.utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setupBottomNavigationView();
+    }
+
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up bottom navigation view");
+
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bnBottomNav);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ app/src/main/java/com/atominize/instagramcloneapp/utils/BottomNavigationViewHelper.java
@@ -0,0 +1,19 @@
+package com.atominize.instagramcloneapp.utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx) {
+        Log.d(TAG, "setupBottomNavigationView: Setting up bottom navigation view");
+
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
