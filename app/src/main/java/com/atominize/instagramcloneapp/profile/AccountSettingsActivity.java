18abf937e4c3d1921bdeb83181485b5d380d7d94
atominize <atominize@gmail.com>
2019-12-28 13:34:11 +0000

    Worked on Account settings List view and finishing activity

--- app/src/main/java/com/atominize/instagramcloneapp/profile/AccountSettingsActivity.java
+++ app/src/main/java/com/atominize/instagramcloneapp/profile/AccountSettingsActivity.java
@@ -2,10 +2,16 @@ package com.atominize.instagramcloneapp.profile;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.atominize.instagramcloneapp.R;
+
+import java.util.ArrayList;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
@@ -17,5 +23,32 @@ public class AccountSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account_settings);
 
         Log.d(TAG, "onCreate: started");
+
+        goBack();
+        setupListView();
+    }
+
+    private void goBack() {
+        ImageView backArrow = findViewById(R.id.ivBack);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating back to Profile Activity");
+                finish();
+            }
+        });
+    }
+
+    private void setupListView() {
+        Log.d(TAG, "setupListView: Initializing account settings list");
+        ListView listView = findViewById(R.id.lvAccountSettings);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(AccountSettingsActivity.this,
+                android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
     }
 }
